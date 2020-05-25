b5d643e00159e8b062a76926d2c67a36b582b1ee
石井諒 <ishiimakoto@markStone.local>
2020-05-25 15:04:19 +0900

    Set up for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+    fetch('/weather?address=' + location).then((res) => {
     res.json().then((data) => {
             p.classList.remove('error');
         if(data.err) {
